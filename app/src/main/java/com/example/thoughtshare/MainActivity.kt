ade419e47ede166fb867a00185c2cc1dc2bf1b5f
raheez66 <raheez66@gmail.com>
2022-05-03 00:55:44 +0530

    first commit

---
+++ app/src/main/java/com/example/thoughtshare/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thoughtshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
