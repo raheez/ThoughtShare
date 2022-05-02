32af972d756412f5a4be3334f6378ddf2e4cf4be
raheez66 <raheez66@gmail.com>
2022-05-03 01:07:32 +0530

    login ui added

--- app/src/main/java/com/example/thoughtshare/MainActivity.kt
+++ app/src/main/java/com/example/thoughtshare/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.thoughtshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.thoughtshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        initListener()
+    }
+
+    private fun initListener() {
+
+        mBinding?.buttonLogin?.setOnClickListener {
+            Toast.makeText(this,"login",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
